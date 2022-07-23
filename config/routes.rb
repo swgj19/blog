7598774565375d8f3d8ed5af7b84e41cbaec2254
swgj19 <swgj19@gmail.com>
2022-07-23 18:48:49 -0400

    Form styling and registration route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get '/user' => "posts#index", :as => :user_root
+  get '/users', to: redirect('/users/sign_up')
   devise_for :users
   resources :users
 
   resources :posts do
     resources :opinions
   end
-  get '/user' => "posts#index", :as => :user_root
+  
   root 'posts#index'
 end
