49e0645807b6816e6ffd83b2772d07912769571d
swgj19 <swgj19@gmail.com>
2022-07-24 19:34:22 -0400

    Profile image upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-      keys: [:username, :name, :email, :password, :password_confirmation])
+      keys: [:username, :name, :email, :password, :password_confirmation, :avatar])
     devise_parameter_sanitizer.permit(:sign_in,
       keys: [:login, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:account_update,
-      keys: [:username, :name, :email, :password_confirmation, :current_password])
+      keys: [:username, :name, :email, :password_confirmation, :current_password, :avatar])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_one_attached :avatar
+  before_commit :add_default_avatar, on: %i[create update]
 
   # shorthand for getter & setter
   attr_accessor :login
@@ -32,6 +34,30 @@ class User < ApplicationRecord
   def validate_username
     if User.where(email: username).exists?
       errors.add(:username, :invalid)
+    end
+  end
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize_to_limit: [50, 50]).processed
+    else
+      '/default_profile.png'
+    end 
+  end
+
+  private
+  
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_profile.png'
+          )
+        ), 
+        filename: 'default_profile.png',
+        content_type: 'image/png'
+      )
     end
   end
 end
